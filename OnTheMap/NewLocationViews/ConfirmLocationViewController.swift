cefff744f2ae70851582d943d769ec2be72ce5bb
Sarah Gunnels Porter <zele.de.cuisine@gmail.com>
2020-06-27 11:11:50 -0400

    Added files for Add and Confirm VCs

---
+++ OnTheMap/NewLocationViews/ConfirmLocationViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ConfirmLocationViewController.swift
+//  OnTheMap
+//
+//  Created by Sarah Gunnels Porter on 6/27/20.
+//  Copyright © 2020 Gunnels Porter. All rights reserved.
+//
+
+import UIKit
+
+class ConfirmLocationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
