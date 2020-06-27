e105fac1c64c3dcca2911c144bd281bb9b2c8831
Sarah Gunnels Porter <zele.de.cuisine@gmail.com>
2020-06-27 12:41:58 -0400

    login button disables clickables and shows activity indicator

--- OnTheMap/LoginViewController.swift
+++ OnTheMap/LoginViewController.swift
@@ -9,12 +9,32 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateViewState(isViewClickable: true)
     }
 
-
+    @IBAction func loginRequested(_ sender: Any) {
+        self.updateViewState(isViewClickable: false)
+    }
+    
+    func updateViewState(isViewClickable: Bool) -> Void {
+        loginButton.isEnabled = isViewClickable
+        emailTextField.isEnabled = isViewClickable
+        passwordTextField.isEnabled = isViewClickable
+        if !isViewClickable {
+            activityIndicator.startAnimating()
+        } else {
+            activityIndicator.stopAnimating()
+        }
+    }
+    
 }
 
